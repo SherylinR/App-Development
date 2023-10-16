11495951de05dcbad85cfb9151390cf6a77b7970
SherylinR <104746922+SherylinR@users.noreply.github.com>
2023-10-16 14:30:12 +0530

    Delete DAY16/Microservices directory

--- DAY16/Microservices/Backend/src/main/java/com/expectopatronum/tamil/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.expectopatronum.tamil.config;
-
-public class JwtConstant {
-
-	public static final String SECRET_KEY="kjhgtyedyesrdtfyhuiytiyuiyitu5ryguky7i6r5ydfhkgytrysdfh";
-	public static final String JWT_HEADER="Authorization";
-}
--- DAY16/Microservices/Feedback/src/main/java/com/example/demo/Repository/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long>{
-    Feedback findByFormId(Long id);
-}
